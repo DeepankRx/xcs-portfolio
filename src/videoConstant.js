eaf25b2d3081304465dd3d160702b873d4168e0a
DeepankRx <deepankpushpad10@gmail.com>
2023-06-21 10:57:53 +0530

    changes

---
+++ src/videoConstant.js
@@ -0,0 +1,48 @@
+import video1 from './assets/1.mp4'
+import video2 from './assets/2.mp4'
+import video3 from './assets/3.mp4'
+import video4 from './assets/4.mp4'
+import video5 from './assets/5.mp4'
+import video6 from './assets/6.mp4'
+import video7 from './assets/7.mp4'
+import video8 from './assets/8.mp4'
+import video9 from './assets/9.mp4'
+import video10 from './assets/10.mp4'
+import video11 from './assets/11.mp4'
+import video12 from './assets/12.mp4'
+import video13 from './assets/13.mp4'
+import video14 from './assets/14.mp4'
+import video15 from './assets/15.mp4'
+import video16 from './assets/16.mp4'
+import video17 from './assets/17.mp4'
+import video18 from './assets/18.mp4'
+import video19 from './assets/19.mp4'
+import video20 from './assets/20.mp4'
+import video21 from './assets/21.mp4'
+import video22 from './assets/22.mp4'
+
+export default [
+    video1,
+    video2,
+    video3,
+    video4,
+    video5,
+    video6,
+    video7,
+    video8,
+    video9,
+    video10,
+    video11,
+    video12,
+    video13,
+    video14,
+    video15,
+    video16,
+    video17,
+    video18,
+    video19,
+    video20,
+    video21,
+    video22,
+]
+
